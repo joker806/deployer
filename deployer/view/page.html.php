1d41cf0c22c7e170647e6b53bf24565a076fb3bc
joker806 <joker806@gmail.com>
2013-07-30 20:35:30 +0200

    Included LESS

--- deployer/view/page.html.php
+++ deployer/view/page.html.php
@@ -7,13 +7,14 @@
 
 	<link href="public/css/bootstrap.min.css" rel="stylesheet">
 	<link href="public/css/font-awesome.min.css" rel="stylesheet">
-	<link href="public/css/app.css" rel="stylesheet">
+	<link href="public/css/app.less" rel="stylesheet/less">
 	<link rel="shortcut icon" href="public/favicon.ico">
 </head>
 
 <body class="<?= isset($bodyClass) ? $bodyClass : '' ?>">
 	<?= $subContent ?>
 
+    <script src="public/js/less.min.js"></script>
 	<script src="public/js/jquery.min.js"></script>
 	<script src="public/js/bootstrap.min.js"></script>
 	<script src="public/js/app.js"></script>
